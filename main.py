2dcab8c5c31e9c65fcef26bdbc463298a8adb1aa
tmoi29 <tmoi@stuy.edu>
2018-04-30 22:22:52 -0400

    revert

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ for i in range(len(words)):
 
 print words
 
-def freq(s):
+def freq_word(s):
     l = [1 for x in words if x == s.lower()]
     if len(l) == 0:
         return 0
     return len(l)
 
-print freq("the")
+print freq_word("the")
 
 def freq_phrase(l):
     l2 = [1 for x in words if x.lower() in l]
@@ -23,6 +23,10 @@ def freq_phrase(l):
 print freq_phrase(["the","ski"])
 
 def most_freq():
-    return reduce( lambda a, b: a if a > freq(b) else freq(b), words)
+    s = set(words)
+    print s
+    l = [freq_word(x) for x in words]
+    i = l.index(max(l))
+    return s[i]
 
 print most_freq()
